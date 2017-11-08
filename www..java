f06f37bc13f4100042e38f64570efea178904ef6
neeraj-5 <33458343+neeraj-5@users.noreply.github.com>
2017-11-08 16:05:38 +0530

    Update www..java

--- www..java
+++ www..java
@@ -2,6 +2,6 @@ public class ForExample {
 2.public static void main(String[] args) {  
 3.    for(int i=1;i<=10;i++){  
 4.        System.out.println(i);  
-5.    }  
+5.    }  for("neeraj")
 6.}  
 7.}  
